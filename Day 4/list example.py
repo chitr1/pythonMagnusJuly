93b8c4d66f45789c1824565d1142dbe9d1be0639
chit1 <citrabal@gmail.com>
2023-07-27 15:16:15 -0500

    26thjuly2023

---
+++ Day 4/list example.py
@@ -0,0 +1,14 @@
+a1 =[10,30,23,'abc',10.5,'hello']
+
+print(a1)
+print("welcome python")
+print (a1[1:4])
+print(a1[1:])
+a1.append(120)
+print(a1)
+a1.insert(2,'welcome')
+print(a1)
+a1[6]="ameya"
+print(a1)
+a1.remove('abc')
+print(a1)
