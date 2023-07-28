ab256e998a6dbc9ca070d906bf8fb8a2a64b83b1
chit1 <citrabal@gmail.com>
2023-07-28 16:28:58 -0500

    27-July 2023

---
+++ Day5/dictonary.py
@@ -0,0 +1,8 @@
+d1={101: 'abc' ,102: 'bcd',103: 'cde'}
+print(type(d1))
+print(d1[101])
+print(d1)
+d1[104]='efg'
+print(d1)
+d1[103]='dfg'
+print(d1)
---
+++ Day5/set collection.py
@@ -0,0 +1,7 @@
+s1={10,20,59,39,60,90,45,60}
+print(s1)
+s1.add(300)
+print(s1)
+s1.remove(39)
+print(s1)
+
