6e68a501e03c4913a4f087e10baf6ffd36702060
chit1 <citrabal@gmail.com>
2023-07-30 13:57:56 -0500

    28-july2023

---
+++ Day6/conditional statment.py
@@ -0,0 +1,16 @@
+a=40
+b=20
+
+if a>b:
+    print("Greater Value")
+
+a=10
+b=40
+if a<b:
+    print("lesser value")
+elif a>b:
+    print("greater value")
+elif a==b:
+     print("equals")
+else:
+    print("invalid")
