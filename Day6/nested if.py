b1f1df5ae9d897bd1ee0ad13938a6bde5a6be497
chit1 <citrabal@gmail.com>
2023-07-30 16:08:08 -0500

    88july2023new

---
+++ Day6/nested if.py
@@ -0,0 +1,7 @@
+username=input("Enter the username")
+password=input("Enter the password")
+if username=="python":
+    if password=="ameya":
+        print("login success")
+    else:
+        print("invalid password")
