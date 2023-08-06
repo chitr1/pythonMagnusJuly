6aa435377ae64d580cd19d4afb2eca193f829323
chit1 <citrabal@gmail.com>
2023-08-06 09:01:11 -0500

    polymorphism

---
+++ Conditional Handson/Problem no-4.py
@@ -0,0 +1,12 @@
+A=int(input("Enter the value of A = "))
+B=int(input("Enter the value of B = "))
+
+c=("score of A ")
+d=("score of A+B")
+
+score=(c+d)
+if("score is greater than 300 and sum is less than 500"):
+    print("can team up")
+
+else:
+    print ("cant team up")
---
+++ Day9/Polymorphism.py
@@ -0,0 +1,29 @@
+class Car_2022:
+    def Roof(self):
+        print("Sunroof")
+
+    def Wheels(self):
+        print("Normal alloy wheels")
+
+    def Music(self):
+        print("7 music touch player")
+
+class car_2023(Car_2022):
+    def Roof(self):
+        #print("panaromic roof")
+        super().Roof()
+
+
+    def Music(self):
+        print("11 music touch player")
+
+    obj1 = car_2023()
+    obj1.Roof()
+    obj1.Wheels()
+    obj1.Music()
+
+
+
+
+
+
