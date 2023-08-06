067ab90e896a7a27700f39c1268c66c47d957240
chit1 <citrabal@gmail.com>
2023-08-06 12:51:31 -0500

    conditional problem no-5

---
+++ Conditional Handson/problem-5.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the triangle side 1 ="))
+b=int(input("Enter the triangle side2 ="))
+c=int(input("Enter the value of side3="))
+
+if a+b+c==180:
+    print("Triangle is valid")
+
+else:
+    print("Triangle not valid")
