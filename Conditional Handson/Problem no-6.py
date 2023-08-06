233636158c8e5e0863124c346dcaea7412545bbb
chit1 <citrabal@gmail.com>
2023-08-06 16:36:58 -0500

    conditional problem no-6

---
+++ Conditional Handson/Problem no-6.py
@@ -0,0 +1,8 @@
+salary=int(input("Enter the salary amount : "))
+years=int(input("Enter the years of service : "))
+
+if years>5:
+    print("bonus granted+5%of salary")
+
+else:
+    print("Bonus not granted")
