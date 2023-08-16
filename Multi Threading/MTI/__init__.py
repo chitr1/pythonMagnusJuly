a5a9c3c0affdfa366be8721f79b936a2f78bb82d
chit1 <citrabal@gmail.com>
2023-08-16 15:04:11 -0500

    Aug15

---
+++ Multi Threading/MTI/__init__.py
@@ -0,0 +1,23 @@
+from threading import Thread
+from time import sleep
+class Sample(Thread):
+    def m1(self):
+        for i in  range(1,21):
+            print(i)
+    def run(self):
+        self.m1()
+
+
+
+class Demo(Thread):
+    def m2(self):
+        for i in range (21,41):
+            print(i)
+    def run(self):
+        self.m2()
+
+obj1=Sample()
+obj2=Demo()
+obj1.start()
+obj2.start()
+
