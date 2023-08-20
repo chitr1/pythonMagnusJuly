f43c6cea7ebdf1d48fd5d96b52ddb33fb4903300
chit1 <citrabal@gmail.com>
2023-08-20 09:30:43 -0500

    Loop Handson

--- looping Handson/Problem #3.py
+++ looping Handson/Problem #3.py
@@ -2,3 +2,5 @@ M=int(input("Enter the number of rows = "))
 N=int(input("Enter the number of Columns = "))
 for i in range (M):
     print(N*"#")
+
+
---
+++ looping Handson/Problem#5.py
@@ -0,0 +1,4 @@
+A=int(input("Enter the value ="))
+for i in range (1,A+1):
+    print(i)
+
