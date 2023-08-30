26d1023527a24deedf286198ea1a844bd275b877
chit1 <citrabal@gmail.com>
2023-08-30 09:49:43 -0500

    Database Handson

--- handson database/Create Tabe.py
+++ handson database/Create Tabe.py
@@ -8,21 +8,31 @@ class Table:
     def create(self):
         c1.execute("create Table student1 (name varchar(20),class int,marks int)")
      def insert(self):
-        c1.execute("insert into student1 values('wik',12,90)")
-        c1.execute("insert into student1 values('Sam',13,92)")
+        c1.execute("insert into student1 values('wik',7,90)")
+        c1.execute("insert into student1 values('Sam',5,92)")
         myDbconnection.commit()
         myDbconnection.close()
 
-
+     def update(self):
+         c1.execute("update student1 set marks=92 where name='wik'")
+         Dbconnection.commit()
+         Dbconnection.close()
+     def alter(self):
+         c1.execute("alter table student1 add age")
+         Dbconnection.close()
     def select(self):
         c1.excecute("select*from students")
         result=fetchall()
         print(result)
+        Dbconnection.close()
 
 
 obj1 = Table()
-obj1.create()
-obj1.insert()
+#obj1.create()
+#obj1.insert()
+#obj1.update()
+#obj1.alter()
+obj1.select()
 
 
 
